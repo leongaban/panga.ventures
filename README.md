91bae57cd25d62bb60cb1d83fe69e2e9a8fe9962
leongaban <leongaban@gmail.com>
2022-01-19 14:08:03 -0600

    basic mvp created

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Panga Ventures website
 
 https://panga.ventures
 
-![Panga Ventures](https://raw.githubusercontent.com/leongaban/github_images/master/panga-header.png)
+![Panga Ventures](https://raw.githubusercontent.com/leongaban/panga.ventures/main/src/imgs/splash.png)
 
 We pride ourselves in personifying the saying
 “fortune favors the brave”
--- src/app.js
+++ src/app.js
@@ -3,23 +3,11 @@ const app = Vue.createApp({
     // template: '<h2>Template</h2>'
     data() {
         return {
-            title: 'The Final Empire',
-            author: 'Brandon',
-            age: '45',
-            css: {
-                headerTitle: {
-                    color: 'red',
-                    fontFamily: 'Nunito',
-                    fontSize: '20px'
-                }
-            }
+        
         }
     },
     methods: {
-        changeTitle() {
-            this.title = 'hello'
-            console.log(`Title is`, this.title)
-        }
+        
     }
 })
 
